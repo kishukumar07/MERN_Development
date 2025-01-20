24be0ad0517bd2b407fcadfea36e9fb8d111529f
kishukumar07 <kishukumars082@gmail.com>
2025-01-20 20:23:35 +0530

    added

---
+++ Backend/05.Database&Mongointro/02Database/concept.js
@@ -0,0 +1,4 @@
+// why Database ?? => Performing crud operation in db files is time consuming although we have to write a lot of logic like FileSystem.read FileSystem,write FileSystem.append and we have manipulate the data then overwrite it ..logics ~ lot of stuffs  (time comsuming /load high )
+
+
+// so in only one line of code makes our crud operations  like :get ,post ,put ,patch ,delete 
