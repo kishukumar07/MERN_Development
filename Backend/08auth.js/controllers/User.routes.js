260d0a78c7c3904683b3a1b6e21f2072f55964ea
Kishukumar07 <kishukumars082@gmail.com>
2025-01-31 22:10:14 +0530

    added

--- Backend/08auth.js/controllers/User.routes.js
+++ Backend/08auth.js/controllers/User.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const userRouter = express.Router();
 const Usermodel = require('../model/user.model')
-
+const jwt = require('jsonwebtoken');
 
 
 userRouter.get('/', (req, res) => {
@@ -27,8 +27,11 @@ const{email,pass}=req.body;  //first you will take the email and pass from front
 try{
     const user = await  Usermodel.findOne({email,pass});  //finding the data related to the given email & pass if it is present in db   {email:emai,pass:passl}==={email,pass} by~ex6
   //findOne will return obj only & if nothing matches there will be null but find will return obj inside array     and if nothing matches there will be  an empty array [] SO lets put some logic here...
-if(user){
-   res.status(200).send({'msg':"Login Sucessful","token":"abc12 3" });   
+if(user){ //authentication
+    var token = jwt.sign({ course: 'backend' }, 'masai');//jwt usage~generating random token (authorisation) weneed to verfy the token in protected routes also in way to acess it ~also verified by jwt 
+
+   res.status(200).send({'msg':"Login Sucessful","token":token});   //one the user authenticated at that point of time we'll generate and asign the token to the clint
+ 
 }else{
 res.status(200).send({"msg":"Wrong Credentials"}); 
 }
--- Backend/08auth.js/script.js
+++ Backend/08auth.js/script.js
@@ -4,6 +4,8 @@ const dotenv=require('dotenv');
 dotenv.config(); 
 
 const {userRouter} =require('./controllers/User.routes')
+const jwt = require('jsonwebtoken');
+
 
 
 const app = express();
@@ -33,23 +35,43 @@ app.get('/about',(req,res)=>{
 
 //AUTHORISED :Protected (bfr entering ,you need to login)
 app.get('/series',(req,res)=>{
-    if(req.query.token==="abc123"){
-        res.end("Series Data"); 
-    }
-    else{
-        res.end("Please login First!!")
-    }
+
+     // verify a token symmetric
+     const {token}=req.query; 
+jwt.verify(token, 'masai', function(err, decoded) {
+   if(decoded){
+    res.status(200).send("series data"); 
+   }else{
+    res.status(400).send({"err":err.message}); 
+   }
+  });
+
+    // if(req.query.token==="abc123"){
+    //     res.end("Series Data"); 
+    // }
+    // else{
+    //     res.end("Please login First!!")
+    // }
    
 })
 
 
 app.get('/movie',(req,res)=>{
-    if(req.query.token==="abc123"){
-        res.end("Movie Data"); 
-    }
-    else{
-        res.end("Please login First!!")
-    }
+    // if(req.query.token==="abc123"){
+    //     res.end("Movie Data"); 
+    // }
+    // else{
+    //     res.end("Please login First!!")
+    // }
+
+    const {token}=req.query; 
+    jwt.verify(token, 'masai', function(err, decoded) {
+       if(decoded){
+        res.status(200).send("movie data"); 
+       }else{
+        res.status(400).send({"err":err.message}); 
+       }
+      });
 })
 
   
