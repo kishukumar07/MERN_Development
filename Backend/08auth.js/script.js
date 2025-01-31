cbe9355029a63650447a11d5688903ad40b67bc4
Kishukumar07 <kishukumars082@gmail.com>
2025-01-31 21:09:30 +0530

    added

---
+++ Backend/08auth.js/script.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const connection = require('./db');
+const dotenv=require('dotenv'); 
+dotenv.config(); 
+
+const {userRouter} =require('./controllers/User.routes')
+
+
+const app = express();
+
+app.use(express.json());// Middleware to parse JSON requests
+
+const port = process.env.port; // Default port
+
+app.use('/users',userRouter);
+
+
+
+//Not authorised :public
+app.get('/',(req,res)=>{
+    res.end("Home page"); 
+})
+app.get('/contacts',(req,res)=>{
+    res.end("Contact page"); 
+})
+
+app.get('/about',(req,res)=>{
+    res.end("about page"); 
+})
+
+
+
+
+//AUTHORISED :Protected (bfr entering ,you need to login)
+app.get('/series',(req,res)=>{
+    if(req.query.token==="abc123"){
+        res.end("Series Data"); 
+    }
+    else{
+        res.end("Please login First!!")
+    }
+   
+})
+
+
+app.get('/movie',(req,res)=>{
+    if(req.query.token==="abc123"){
+        res.end("Movie Data"); 
+    }
+    else{
+        res.end("Please login First!!")
+    }
+})
+
+  
+
+
+
+
+
+app.listen(port,  async() => {
+    try {
+          await connection
+        console.log(" Server connected to db")
+    } catch (err) {
+        console.log(err);
+        console.log("err Occured at db connection");  
+    }
+    console.log(`Server is running on port ${port}`);
+});
+
