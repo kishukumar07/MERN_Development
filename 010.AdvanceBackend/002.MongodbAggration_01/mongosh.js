bde17d4d8bd0a61b117541217980792901712ec7
Kishukumar07 <kishukumars082@gmail.com>
2025-02-19 19:00:15 +0530

    MongoAggregation.js

--- 010.AdvanceBackend/002.MongodbAggration_01/mongosh.js
+++ 010.AdvanceBackend/002.MongodbAggration_01/mongosh.js
@@ -1,4 +1,4 @@
-// INSERTED THE DATA SET TO COLLECTION 
+// INSERTED THE DATA SET TO COLLECTION     -------Practical of mongodb aggrigation operator  
 
 mongoAggregation > db.orders.insertMany([
   ...  { _id: 0, name: "Pepperoni", size: "small", price: 19, quantity: 10, date: ISODate("2021-03-13T08:14:30Z") },
@@ -185,12 +185,12 @@ mongoAggregation> db.orders.find().limit(2)
 
 //we want only 2 documents from top 
 mongoAggregation> db.orders.aggregate([{}])
-MongoServerError[Location40323] : A pipeline stage specification object must contain exactly one field.
+// MongoServerError[Location40323] : A pipeline stage specification object must contain exactly one field.
 
 
 
 
-//pagination with $limit in aggregation 
+//pagination with $limit in aggregation    $limit
 mongoAggregation> db.orders.aggregate([{$limit:2}])    
 [
   {
@@ -212,11 +212,205 @@ mongoAggregation> db.orders.aggregate([{$limit:2}])
 ]
 
 
+//SORTING IN THE BASIS OF A FIELD'S VALUE with general .find().sort({field:-1/1}) 
+mongoAggregation> db.orders.find({}).sort({price:-1})
+[
+  {
+    _id: 2,
+    name: 'Pepperoni',
+    size: 'large',
+    price: 21,
+    quantity: 30,
+    date: ISODate('2021-03-17T09:22:12.000Z')
+  },
+  {
+    _id: 1,
+    name: 'Pepperoni',
+    size: 'medium',
+    price: 20,
+    quantity: 20,
+    date: ISODate('2021-03-13T09:13:24.000Z')
+  },
+  {
+    _id: 0,
+    name: 'Pepperoni',
+    size: 'small',
+    price: 19,
+    quantity: 10,
+    date: ISODate('2021-03-13T08:14:30.000Z')
+  },
+  {
+    _id: 7,
+    name: 'Vegan',
+    size: 'medium',
+    price: 18,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:10:13.000Z')
+  },
+  {
+    _id: 6,
+    name: 'Vegan',
+    size: 'small',
+    price: 17,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:08:13.000Z')
+  },
+  {
+    _id: 5,
+    name: 'Cheese',
+    size: 'large',
+    price: 14,
+    quantity: 10,
+    date: ISODate('2022-01-12T05:08:13.000Z')
+  },
+  {
+    _id: 4,
+    name: 'Cheese',
+    size: 'medium',
+    price: 13,
+    quantity: 50,
+    date: ISODate('2022-01-12T21:23:13.331Z')
+  },
+  {
+    _id: 3,
+    name: 'Cheese',
+    size: 'small',
+    price: 12,
+    quantity: 15,
+    date: ISODate('2021-03-13T11:21:39.736Z')
+  }
+]
+
+
+//Aggrigation operator -$sort....SORTING IN THE BASIS OF A FIELD'S VALUE
+mongoAggregation> db.orders.aggregate([{$sort:{"price":-1}}])  //-1(decending)...1(Ascending)
+[
+  {
+    _id: 2,
+    name: 'Pepperoni',
+    size: 'large',
+    price: 21,
+    quantity: 30,
+    date: ISODate('2021-03-17T09:22:12.000Z')
+  },
+  {
+    _id: 1,
+    name: 'Pepperoni',
+    size: 'medium',
+    price: 20,
+    quantity: 20,
+    date: ISODate('2021-03-13T09:13:24.000Z')
+  },
+  {
+    _id: 0,
+    name: 'Pepperoni',
+    size: 'small',
+    price: 19,
+    quantity: 10,
+    date: ISODate('2021-03-13T08:14:30.000Z')
+  },
+  {
+    _id: 7,
+    name: 'Vegan',
+    size: 'medium',
+    price: 18,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:10:13.000Z')
+  },
+  {
+    _id: 6,
+    name: 'Vegan',
+    size: 'small',
+    price: 17,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:08:13.000Z')
+  },
+  {
+    _id: 5,
+    name: 'Cheese',
+    size: 'large',
+    price: 14,
+    quantity: 10,
+    date: ISODate('2022-01-12T05:08:13.000Z')
+  },
+  {
+    _id: 4,
+    name: 'Cheese',
+    size: 'medium',
+    price: 13,
+    quantity: 50,
+    date: ISODate('2022-01-12T21:23:13.331Z')
+  },
+  {
+    _id: 3,
+    name: 'Cheese',
+    size: 'small',
+    price: 12,
+    quantity: 15,
+    date: ISODate('2021-03-13T11:21:39.736Z')
+  }
+]
+
+
+//Both limit and sort together with .sort() and .limit()
+mongoAggregation> db.orders.find({}).limit(3).sort({_id:-1})
+[
+  {
+    _id: 7,
+    name: 'Vegan',
+    size: 'medium',
+    price: 18,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:10:13.000Z')
+  },
+  {
+    _id: 6,
+    name: 'Vegan',
+    size: 'small',
+    price: 17,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:08:13.000Z')
+  },
+  {
+    _id: 5,
+    name: 'Cheese',
+    size: 'large',
+    price: 14,
+    quantity: 10,
+    date: ISODate('2022-01-12T05:08:13.000Z')
+  }
+]
 
 
 
 
 
 
-
-
+//Both limit and sort together with aggregation operators $limit and $sort
+mongoAggregation> db.orders.aggregate([{$limit:3},{$sort:{_id:-1}}])
+[
+  {
+    _id: 2,
+    name: 'Pepperoni',
+    size: 'large',
+    price: 21,
+    quantity: 30,
+    date: ISODate('2021-03-17T09:22:12.000Z')
+  },
+  {
+    _id: 1,
+    name: 'Pepperoni',
+    size: 'medium',
+    price: 20,
+    quantity: 20,
+    date: ISODate('2021-03-13T09:13:24.000Z')
+  },
+  {
+    _id: 0,
+    name: 'Pepperoni',
+    size: 'small',
+    price: 19,
+    quantity: 10,
+    date: ISODate('2021-03-13T08:14:30.000Z')
+  }
+]
