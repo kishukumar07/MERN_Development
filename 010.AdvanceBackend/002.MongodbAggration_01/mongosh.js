150f30f21beab705cfc2838310818eedc29bc7b3
Kishukumar07 <kishukumars082@gmail.com>
2025-02-19 17:48:03 +0530

    MongoAggregation.js

--- 010.AdvanceBackend/002.MongodbAggration_01/mongosh.js
+++ 010.AdvanceBackend/002.MongodbAggration_01/mongosh.js
@@ -10,16 +10,16 @@ mongoAggregation > db.orders.insertMany([
 
   ...   { _id: 6, name: "Vegan", size: "small", price: 17, quantity: 10, date: ISODate("2021-01-13T05:08:13Z") },
   ...     { _id: 7, name: "Vegan", size: "medium", price: 18, quantity: 10, date: ISODate("2021-01-13T05:10:13Z") }
-... ])
+ ]),
 {
   acknowledged: true,
-    insertedIds: { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7 }
+    insertedIds : { '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7 }
 }
 
 
 
 
-//IT IS HERE OR NOT   
+//jUST GOING TO THE DATA   SO WE CAN FAMILIER WITH IT  by going different field here ....
 mongoAggregation > db.orders.find()    
 [
   {
@@ -87,8 +87,136 @@ mongoAggregation > db.orders.find()
     date: ISODate('2021-01-13T05:10:13.000Z')
   }
 
+]
+
+
+
+//THE SAME THING WITH AGGRIGATION-( FETCHING ALL DOCUMENTS OF THE COLLECTION )
+mongoAggregation> db.orders.aggregate([])    //REMEMBER  THE SYNTEX ??.... 
+[
+  {
+    _id: 0,
+    name: 'Pepperoni',
+    size: 'small',
+    price: 19,
+    quantity: 10,
+    date: ISODate('2021-03-13T08:14:30.000Z')
+  },
+  {
+    _id: 1,
+    name: 'Pepperoni',
+    size: 'medium',
+    price: 20,
+    quantity: 20,
+    date: ISODate('2021-03-13T09:13:24.000Z')
+  },
+  {
+    _id: 2,
+    name: 'Pepperoni',
+    size: 'large',
+    price: 21,
+    quantity: 30,
+    date: ISODate('2021-03-17T09:22:12.000Z')
+  },
+  {
+    _id: 3,
+    name: 'Cheese',
+    size: 'small',
+    price: 12,
+    quantity: 15,
+    date: ISODate('2021-03-13T11:21:39.736Z')
+  },
+  {
+    _id: 4,
+    name: 'Cheese',
+    size: 'medium',
+    price: 13,
+    quantity: 50,
+    date: ISODate('2022-01-12T21:23:13.331Z')
+  },
+  {
+    _id: 5,
+    name: 'Cheese',
+    size: 'large',
+    price: 14,
+    quantity: 10,
+    date: ISODate('2022-01-12T05:08:13.000Z')
+  },
+  {
+    _id: 6,
+    name: 'Vegan',
+    size: 'small',
+    price: 17,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:08:13.000Z')
+  },
+  {
+    _id: 7,
+    name: 'Vegan',
+    size: 'medium',
+    price: 18,
+    quantity: 10,
+    date: ISODate('2021-01-13T05:10:13.000Z')
+  }
+]  //we just written db.orders.aggregate([]) and an empty array 
+
+
+//Pagination 
+mongoAggregation> db.orders.find().limit(2)
+[
+  {
+    _id: 0,
+    name: 'Pepperoni',
+    size: 'small',
+    price: 19,
+    quantity: 10,
+    date: ISODate('2021-03-13T08:14:30.000Z')
+  },
+  {
+    _id: 1,
+    name: 'Pepperoni',
+    size: 'medium',
+    price: 20,
+    quantity: 20,
+    date: ISODate('2021-03-13T09:13:24.000Z')
+  }
+]
+
+
+//we want only 2 documents from top 
+mongoAggregation> db.orders.aggregate([{}])
+MongoServerError[Location40323] : A pipeline stage specification object must contain exactly one field.
 
 
 
 
-  
+//pagination with $limit in aggregation 
+mongoAggregation> db.orders.aggregate([{$limit:2}])    
+[
+  {
+    _id: 0,
+    name: 'Pepperoni',
+    size: 'small',
+    price: 19,
+    quantity: 10,
+    date: ISODate('2021-03-13T08:14:30.000Z')
+  },
+  {
+    _id: 1,
+    name: 'Pepperoni',
+    size: 'medium',
+    price: 20,
+    quantity: 20,
+    date: ISODate('2021-03-13T09:13:24.000Z')
+  }
+]
+
+
+
+
+
+
+
+
+
+
