83af5532eb68cbcdf1496646d6ed578b01a4d992
Kishukumar07 <kishukumars082@gmail.com>
2025-03-16 20:13:23 +0530

    reduce problem 4

--- 003.AdvanceJsConcepts/ArrayMethods/Reduce in depth/arr.reduce.js
+++ 003.AdvanceJsConcepts/ArrayMethods/Reduce in depth/arr.reduce.js
@@ -73,5 +73,32 @@ function getAvgAge(users) {
 }
 
 
+//Problem4. Let’s say we received an array of users in the form {id:..., name:..., age:...}. Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values....
+
+let users_2 = [
+    { id: 'john', name: "John Smith", age: 20 },
+    { id: 'ann', name: "Ann Smith", age: 24 },
+    { id: 'pete', name: "Pete Peterson", age: 31 },
+];
+
+groupById(users_2); 
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+
+groupById = (arr) => {
+
+
+
+
+}
+
 
 
