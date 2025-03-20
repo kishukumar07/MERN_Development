e64b3ab83d3d9c385efd8c6e65505605ab453590
Kishukumar07 <kishukumars082@gmail.com>
2025-03-20 09:39:16 +0530

    added sort parm returns theory

--- 003.AdvanceJsConcepts/ArrayMethods/Array.Sort/script.js
+++ 003.AdvanceJsConcepts/ArrayMethods/Array.Sort/script.js
@@ -5,4 +5,34 @@ const months = ["March", "Jan", "Feb", "Dec"];
 months.sort();
 console.log(months);
 // Expected output: Array ["Dec", "Feb", "Jan", "March"]
-...
+
+
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();  //this works by comparing only first place of the elements ? leads to unwanted results ... 
+console.log(array1);
+// Expected output: Array [1, 100000, 21, 30, 4]
+
+
+
+
+/* 
+Syntax-js
+ A.   sort()          
+
+ B.    sort(compareFn)
+                      ~A function that determines the order of the elements. the function is called with the following arguments   
+                             (a,b)=>{         }
+                      a ~ the first element for comparison . will never be undefined . 
+                      
+                      b~ the second element for comparision. will never be undefined. 
+
+        what the callback as a param returns ? 
+                 
+               It should return a number where:
+                         *A negative value indicates that a should come   before b.
+                         *A positive value indicates that a should come     
+                          after b.
+                         *Zero or NaN indicates that a and b are considered equal.
+
+*/
