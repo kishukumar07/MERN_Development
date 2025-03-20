bd15291083f07ff40291884704b78ed2ed9eb6fd
Kishukumar07 <kishukumars082@gmail.com>
2025-03-20 12:54:19 +0530

    solved

---
+++ 003.AdvanceJsConcepts/ArrayMethods/Array.Sort/script7.js
@@ -0,0 +1,9 @@
+const arrayLike = {
+    length: 3,
+    unrelated: "foo",
+    0: 5,
+    2: 4,
+  };
+  console.log(Array.prototype.sort.call(arrayLike));
+  // { '0': 4, '1': 5, length: 3, unrelated: 'foo' }
+  
