912b11419dc2131c75da3062c03ea4d653100fac
Kishukumar07 <kishukumars082@gmail.com>
2025-03-20 08:16:17 +0530

    solving arr.sort logic indepth

---
+++ 003.AdvanceJsConcepts/ArrayMethods/Array.Sort/script.js
@@ -0,0 +1,7 @@
+//Defination :  The sort() method of Array instances sorts the elements of an array in place and returns the reference to the same array, now sorted..
+// The toSorted() method of Array instances is the copying version of the sort() method. It returns a new array with the elements sorted in ascending order.
+
+const months = ["March", "Jan", "Feb", "Dec"];
+months.sort();
+console.log(months);
+// Expected output: Array ["Dec", "Feb", "Jan", "March"]
