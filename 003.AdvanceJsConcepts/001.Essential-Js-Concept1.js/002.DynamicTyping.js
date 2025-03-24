b2ec8b4b79a7175c3320aa0cf9c9f10e0752e9ec
Kishukumar07 <kishukumars082@gmail.com>
2025-03-24 21:05:44 +0530

    dynamic typed language

---
+++ 003.AdvanceJsConcepts/001.Essential-Js-Concept1.js/002.DynamicTyping.js
@@ -0,0 +1,15 @@
+// why js is a dynamic typed programming language ?
+/* 
+let xyz = 'Vivek';
+let xyz = 1;
+let xyz = true;
+let xyz = undefined;
+let xyz = 1234567890123456789012345678901234567890n;
+let xyz = Symbol('hello');
+let xyz = null; // a bug in JS :)
+
+let xyz = {name: 'Vivek'};
+let xyz = [1,2,3]; // you should use Array.isArray(xyz) and not typeof
+let xyz = function(){console.log('Hello')}
+
+*/ 
