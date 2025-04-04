b82bd07368ba339730bccc5971889264ee2b80b5
Kishukumar07 <kishukumars082@gmail.com>
2025-04-04 21:36:57 +0530

    basic logger establized

--- 011.AdvanceBackend02.js/02.monitoring_and_logging/logger/index.js
+++ 011.AdvanceBackend02.js/02.monitoring_and_logging/logger/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { transports, format, transport } from 'winston';
+import pkg from 'winston';
+const { transports, format, transport } =pkg;
 import expressWinston from 'express-winston';
 
 
@@ -8,27 +9,42 @@ const app = express();
 
 
 app.use(expressWinston.logger({
+
+    //transports are basically where we log(record )the logs ...
     transports: [
         new transports.File({
             level: 'error',
             filename: 'errorlogs.log'
+        }),
+        //we can add as many  transports we wants ...
+        new transports.File({
+            level: 'info',
+            filename: 'infologs.log'
         })
-//we can add as many  transports we wants ...
-    ]
-
-}));
-
-
-
-
-
-
-
+    ],
+    //format :how u want the data 
+    format: format.combine(
+        format.json(),
+        format.prettyPrint(),
+        format.timestamp()
+    )
+}) //thats it we have aded the logger 
+);
 
 
 
 
+app.get('/',(req,res)=>{
 
+    res.send('hii this niket logger'); 
+
+})
+
+
+
+app.listen(8080,()=>{
+    console.log('server is started at 8080')
+}); 
 
 
 
