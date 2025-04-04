f0e3324a7afd3a7f528de4b4b66dcd0ac276ba49
Kishukumar07 <kishukumars082@gmail.com>
2025-04-04 21:11:49 +0530

    preparing for logger middle ware

--- 011.AdvanceBackend02.js/02.monitoring_and_logging/logger/index.js
+++ 011.AdvanceBackend02.js/02.monitoring_and_logging/logger/index.js
@@ -1,15 +1,22 @@
-// const express =require('express'); 
-
-
-// console.log(express()); 
-
-
+import express from 'express';
+import { transports, format, transport } from 'winston';
+import expressWinston from 'express-winston';
 
 
 
+const app = express();
 
 
+app.use(expressWinston.logger({
+    transports: [
+        new transports.File({
+            level: 'error',
+            filename: 'errorlogs.log'
+        })
+//we can add as many  transports we wants ...
+    ]
 
+}));
 
 
 
